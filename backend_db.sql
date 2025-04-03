71777ad934c414a48645c254e49cee5d607fcdfd
马楚喆 <machuzhe@machuzhedeMacBook-Pro.local>
2025-04-04 10:46:08 +1100

    new sql

--- backend_db.sql
+++ backend_db.sql
@@ -11,14 +11,11 @@
  Target Server Version : 80035 (8.0.35)
  File Encoding         : 65001
 
- Date: 21/03/2025 18:45:30
+ Date: 04/04/2025 10:43:15
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
-CREATE DATABASE IF NOT EXISTS comp9900_db;
-
 
 -- ----------------------------
 -- Table structure for Event
@@ -32,33 +29,20 @@ CREATE TABLE `Event` (
   `IMG` blob,
   `External_Link` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
 -- ----------------------------
--- Records of Event
+-- Table structure for friends
 -- ----------------------------
-BEGIN;
-COMMIT;
-
--- ----------------------------
--- Table structure for Friends
--- ----------------------------
-DROP TABLE IF EXISTS `Friends`;
-CREATE TABLE `Friends` (
+DROP TABLE IF EXISTS `friends`;
+CREATE TABLE `friends` (
+  `FID` int NOT NULL AUTO_INCREMENT,
   `UID` int NOT NULL,
   `Friend_ID` int NOT NULL,
   `Status` enum('pending','accept','reject') NOT NULL,
-  PRIMARY KEY (`UID`,`Friend_ID`),
-  KEY `Friend_ID` (`Friend_ID`),
-  CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `User` (`ID`) ON DELETE CASCADE,
-  CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`Friend_ID`) REFERENCES `User` (`ID`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- ----------------------------
--- Records of Friends
--- ----------------------------
-BEGIN;
-COMMIT;
+  PRIMARY KEY (`FID`),
+  KEY `Friend_ID` (`Friend_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 
 -- ----------------------------
 -- Table structure for Message
@@ -76,13 +60,7 @@ CREATE TABLE `Message` (
   KEY `receiver_ID` (`receiver_ID`),
   CONSTRAINT `message_ibfk_1` FOREIGN KEY (`sender_ID`) REFERENCES `User` (`ID`) ON DELETE CASCADE,
   CONSTRAINT `message_ibfk_2` FOREIGN KEY (`receiver_ID`) REFERENCES `User` (`ID`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- ----------------------------
--- Records of Message
--- ----------------------------
-BEGIN;
-COMMIT;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
 -- ----------------------------
 -- Table structure for User
@@ -91,11 +69,11 @@ DROP TABLE IF EXISTS `User`;
 CREATE TABLE `User` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `Name` varchar(20) NOT NULL,
-  `Level_of_study` enum('undergraduate','postgraduate','phd','research') NOT NULL,
-  `Email` varchar(40) NOT NULL,
-  `Password_hash` text NOT NULL,
+  `Level_of_study` enum('undergraduate','postgraduate','phd','research') CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `Email` varchar(40) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `Password_hash` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
   `Created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `User_Type` enum('admin','normal') NOT NULL DEFAULT 'normal',
+  `User_Type` enum('admin','normal') CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT 'normal',
   `User_city` varchar(40) DEFAULT NULL,
   `User_country` varchar(40) DEFAULT NULL,
   `User_Field` varchar(40) DEFAULT NULL,
@@ -104,13 +82,7 @@ CREATE TABLE `User` (
   `User_Uni` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `Email` (`Email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- ----------------------------
--- Records of User
--- ----------------------------
-BEGIN;
-COMMIT;
+) ENGINE=InnoDB AUTO_INCREMENT=5529177 DEFAULT CHARSET=utf8mb3;
 
 -- ----------------------------
 -- Table structure for User_Event
@@ -125,11 +97,5 @@ CREATE TABLE `User_Event` (
   CONSTRAINT `user_event_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `User` (`ID`) ON DELETE CASCADE,
   CONSTRAINT `user_event_ibfk_2` FOREIGN KEY (`EID`) REFERENCES `Event` (`ID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- ----------------------------
--- Records of User_Event
--- ----------------------------
-BEGIN;
-COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
